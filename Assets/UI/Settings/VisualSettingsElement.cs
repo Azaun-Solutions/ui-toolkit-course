fc62d6fa8e5372a06735cece23334a4d6f5c3643
unknown <corymaklin@gmail.com>
2021-10-09 18:59:03 -0400

    5 Settings Screen

---
+++ Assets/UI/Settings/VisualSettingsElement.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI.Settings
+{
+    public class VisualSettingsElement : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<VisualSettingsElement, UxmlTraits> {}
+
+        private Label m_ResolutionLabel;
+        private Label m_FullScreenLabel;
+
+        private int m_CurrentResolutionIndex;
+        private bool m_IsFullScreen;
+
+        public VisualSettingsElement()
+        {
+            VisualTreeAsset visualTree = Resources.Load<VisualTreeAsset>("UI/Settings/VisualSettings");
+            visualTree.CloneTree(this);
+
+            m_ResolutionLabel = this.Q<Label>("resolution-label");
+            m_FullScreenLabel = this.Q<Label>("full-screen-label");
+            Button previousResolutionButton = this.Q<Button>("previous-resolution-button");
+            previousResolutionButton.clicked += PreviousResolution;
+            Button nextResolutionButton = this.Q<Button>("next-resolution-button");
+            nextResolutionButton.clicked += NextResolution;
+            Button previousFullScreenButton = this.Q<Button>("previous-full-screen-button");
+            previousFullScreenButton.clicked += PreviousFullScreen;
+            Button nextFullScreenButton = this.Q<Button>("next-full-screen-button");
+            nextFullScreenButton.clicked += NextFullScreen;
+
+            m_IsFullScreen = Screen.fullScreen;
+            SetFullScreenField();
+
+            m_CurrentResolutionIndex = Array.FindIndex(Screen.resolutions,
+                resolution => resolution.width == Screen.currentResolution.width &&
+                              resolution.height == Screen.currentResolution.height);
+            OnResolutionChanged();
+        }
+
+        private void NextFullScreen()
+        {
+            m_IsFullScreen = true;
+            OnFullScreenChanged();
+        }
+
+        private void PreviousFullScreen()
+        {
+            m_IsFullScreen = false;
+            OnFullScreenChanged();
+        }
+
+        private void OnFullScreenChanged()
+        {
+            Screen.fullScreen = m_IsFullScreen;
+            SetFullScreenField();
+        }
+
+        private void SetFullScreenField()
+        {
+            m_FullScreenLabel.text = m_IsFullScreen ? "On" : "Off";
+        }
+
+        private void NextResolution()
+        {
+            m_CurrentResolutionIndex = Mathf.Clamp(m_CurrentResolutionIndex + 1, 0, Screen.resolutions.Length - 1);
+            OnResolutionChanged();
+        }
+
+        private void PreviousResolution()
+        {
+            m_CurrentResolutionIndex = Mathf.Clamp(m_CurrentResolutionIndex - 1, 0, Screen.resolutions.Length - 1);
+            OnResolutionChanged();
+        }
+
+        private void OnResolutionChanged()
+        {
+            Resolution resolution = Screen.resolutions[m_CurrentResolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, m_IsFullScreen);
+            SetResolutionField();
+        }
+
+        private void SetResolutionField()
+        {
+            string displayText = Screen.resolutions[m_CurrentResolutionIndex].ToString();
+            displayText = displayText.Substring(0, displayText.IndexOf("@"));
+            m_ResolutionLabel.text = displayText;
+        }
+    }
+}
