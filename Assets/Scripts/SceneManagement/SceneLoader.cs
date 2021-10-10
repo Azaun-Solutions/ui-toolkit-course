08561ebeb3925d759d19a9e3b7885541f9612c7d
unknown <corymaklin@gmail.com>
2021-10-10 07:09:29 -0400

    3 Main Menu

---
+++ Assets/Scripts/SceneManagement/SceneLoader.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace SceneManagement
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField] private LoadSceneChannel m_LoadSceneChannel;
+
+        private SceneReference m_SceneToLoad;
+        private SceneReference m_CurrentSceneReference;
+
+        private void OnEnable()
+        {
+            m_LoadSceneChannel.load += OnLoadScene;
+        }
+
+        private void OnDisable()
+        {
+            m_LoadSceneChannel.load -= OnLoadScene;
+        }
+
+        private void OnLoadScene(SceneReference sceneReference)
+        {
+            m_SceneToLoad = sceneReference;
+
+            if (m_CurrentSceneReference != null)
+            {
+                m_CurrentSceneReference.UnLoadScene();
+            }
+
+            AsyncOperationHandle<SceneInstance> handle = sceneReference.LoadSceneAsync(LoadSceneMode.Additive);
+            handle.Completed += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(AsyncOperationHandle<SceneInstance> handle)
+        {
+            Scene scene = handle.Result.Scene;
+            SceneManager.SetActiveScene(scene);
+            m_CurrentSceneReference = m_SceneToLoad;
+        }
+    }
+}
